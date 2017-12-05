8b82233a6621620721ff158b02f4f18ce061ae3c
Connor W <connor@blackhawksoftware.net>
2017-12-05 11:59:47 +0000

    Basic server functionality

--- Hawkchat/Server/Server.cs
+++ Hawkchat/Server/Server.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Hawkchat.Server
 {
@@ -39,11 +40,37 @@ namespace Hawkchat.Server
 
         SimpleTcpServer server;
 
-        private static void ServerWorker_DoWork(object sender, DoWorkEventArgs e)
+        //todo: add pool of servers?
+
+        private void ServerWorker_DoWork(object sender, DoWorkEventArgs e)
         {
 
+            server = new SimpleTcpServer().Start(3289);
 
+            Utils.CyanWriteLine("[INIT] Server listening on port 3289.");
 
+            server.ClientConnected += Server_ClientConnected;
+            server.ClientDisconnected += Server_ClientDisconnected;
+
+            server.DataReceived += Server_DataReceived;
+
+        }
+
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+
+            MessageBox.Show(e.MessageString);
+
+        }
+
+        private void Server_ClientDisconnected(object sender, System.Net.Sockets.TcpClient e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Server_ClientConnected(object sender, System.Net.Sockets.TcpClient e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
