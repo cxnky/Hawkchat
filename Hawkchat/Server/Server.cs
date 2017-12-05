d7c31695f23cb2aee3cf57e6c4bc2c1172189f25
Connor W <connor@blackhawksoftware.net>
2017-12-05 11:56:55 +0000

    Added server thread code

--- Hawkchat/Server/Server.cs
+++ Hawkchat/Server/Server.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Server.utils;
+using SimpleTCP;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,39 @@ namespace Hawkchat.Server
 {
     class Server
     {
+
+        private static BackgroundWorker serverWorker; 
+        
         static void Main(string[] args)
         {
+
+            new Server().Start();
+
+        }
+
+        private void Start()
+        {
+
+            Utils.CyanWriteLine("[INIT] Starting server thread...");
+
+            serverWorker = new BackgroundWorker();
+
+            serverWorker.DoWork += ServerWorker_DoWork;
+
+            serverWorker.WorkerSupportsCancellation = true;
+            serverWorker.WorkerReportsProgress = false;
+
+            serverWorker.RunWorkerAsync();
+
+        }
+
+        SimpleTcpServer server;
+
+        private static void ServerWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+
+
         }
     }
 }
---
+++ Hawkchat/Server/utils/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Console = Colorful.Console;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.utils
+{
+    public class Utils
+    {
+
+        public static Color CYAN = Color.Cyan;
+
+        public static void CyanWriteLine(string text)
+        {
+
+            Console.WriteLine(text, CYAN);
+
+        }
+
+    }
+}
