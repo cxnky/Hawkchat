87a19b6ceeb8af35aa0f3462fdd3d17fe0015b9e
Connor W <connor@blackhawksoftware.net>
2017-12-11 15:42:27 +0000

    Updated assembly info

--- Hawkchat/Client/Properties/AssemblyInfo.cs
+++ Hawkchat/Client/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Hawk Chat")]
+[assembly: AssemblyDescription("A free encrypted P2P chat program support a variety of features")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Client")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("BlackHawk Software")]
+[assembly: AssemblyProduct("Hawk Chat")]
+[assembly: AssemblyCopyright("Copyright © BlackHawk Software 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
