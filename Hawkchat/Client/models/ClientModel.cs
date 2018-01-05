30da47d056824291108eec22700c8ae3437fb5b0
Connor W <connor@blackhawksoftware.net>
2018-01-05 14:26:46 +0000

    Added modified ClientModel to client for incoming conversations

---
+++ Hawkchat/Client/models/ClientModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hawkchat.Client.Models
+{
+    public class ClientModel
+    {
+
+        public TcpClient TCPClient { get; set; }
+
+        public string IP { get; set; }
+
+        public int Port { get; set; }
+
+        public string Username { get; set; }
+
+        public string AvatarURL { get; set; }
+
+        public Int64 UserID { get; set; }
+
+    }
+}
