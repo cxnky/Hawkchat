f0a64ebf298aa3b8039f8511644a3ed57518c626
Connor W <connor@blackhawksoftware.net>
2018-01-05 14:27:11 +0000

    Changed tuple param from colour to visuallabel

--- Hawkchat/Client/utils/Constants.cs
+++ Hawkchat/Client/utils/Constants.cs
@@ -4,6 +4,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using VisualPlus.Toolkit.Controls.Interactivity;
 
 namespace Hawkchat.Client.utils
 {
@@ -14,9 +16,11 @@ namespace Hawkchat.Client.utils
         public static Size LARGE_WINDOW = new Size(883, 538);
 
         public static Color OFFLINE_COLOUR = Color.FromArgb(1, 255, 0, 0);
-        public static Color ONLINE_COLOUR = Color.FromArgb(1, 0, 255, 0);
+        public static Color ONLINE_COLOUR = Color.FromArgb(0, 0, 255, 0);
         public static Color AWAY_COLOUR = Color.FromArgb(1, 249, 215, 0);
         public static Color DND_COLOUR = Color.FromArgb(1, 255, 165, 0);
+
+        public static VisualLabel OFFLINE_LABEL, ONLINE_LABEL, AWAY_LABEL, DND_LABEL;
 
     }
 }
--- Hawkchat/Client/utils/Utils.cs
+++ Hawkchat/Client/utils/Utils.cs
@@ -7,32 +7,33 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using VisualPlus.Toolkit.Controls.Interactivity;
 
 namespace Hawkchat.Client.utils
 {
     public class Utils
     {
 
-        public static Tuple<string, Color> GetUserStatusText(UserStatus status)
+        public static Tuple<string, VisualLabel> GetUserStatusText(UserStatus status)
         {
 
             switch (status)
             {
 
                 case UserStatus.OFFLINE:
-                    return new Tuple<string, Color>("Offline", Constants.OFFLINE_COLOUR);
+                    return new Tuple<string, VisualLabel>("Offline", Constants.OFFLINE_LABEL);
 
                 case UserStatus.ONLINE:
-                    return new Tuple<string, Color>("Online", Constants.ONLINE_COLOUR);
+                    return new Tuple<string, VisualLabel>("Online", Constants.ONLINE_LABEL);
 
                 case UserStatus.AWAY:
-                    return new Tuple<string, Color>("Away", Constants.AWAY_COLOUR);
+                    return new Tuple<string, VisualLabel>("Away", Constants.AWAY_LABEL);
 
                 case UserStatus.DND:
-                    return new Tuple<string, Color>("DND", Constants.DND_COLOUR);
+                    return new Tuple<string, VisualLabel>("DND", Constants.DND_LABEL);
 
                 default:
-                    return new Tuple<string, Color>("Unknown", Constants.OFFLINE_COLOUR);
+                    return new Tuple<string, VisualLabel>("Unknown", Constants.OFFLINE_LABEL);
 
             }
 
